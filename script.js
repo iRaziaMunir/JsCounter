2db77190d259102069c6a373652bc96b0eb37f04
Razia Munir <irazia.munir@gmail.com>
2023-08-08 23:40:50 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+let count =0;
+const counter = document.getElementById("count");
+const addBtn = document.getElementById("addCountBt");
+const subtractBtn = document.getElementById("subtractCountBt");
+counter.innerText = count;
+
+
+addBtn.addEventListener("click",()=>{
+    count ++; 
+    counter.innerText = count;
+})
